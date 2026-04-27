b305d1a9ab2e9b762b3d77fd3a88a520b8b79394
Shreekar Pujari <shreekar.up@gmail.com>
2026-04-27 18:27:31 +0530

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int m = -1, n = -1;
+
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+
+        for(int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1); 
+        }
+        for(int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X'; // surrounded
+                if (board[i][j] == 'T') board[i][j] = 'O'; // safe, restore
+            }
+        }
+    }
+
+    void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != 'O') {
+            return;
+        }
+
+        grid[i][j] = 'T';
+
+        for(int[] dir : dirs) {
+            int nr = i + dir[0], nc = j + dir[1];
+
+            dfs(grid, nr, nc);
+        }
+    }
+}
